4d4fd2350c8dc02748dce17877db8db62ba89a9a
Sven A. Schmidt <sas@abstracture.de>
2012-04-04 15:29:50 +0200

    Displaying projects

--- elme/MasterViewController.m
+++ elme/MasterViewController.m
@@ -168,45 +168,48 @@
 }
 
 
-#pragma mark - Table View
+#pragma mark - CouchUITableDelegate
 
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-  return 1;
-}
-
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return _objects.count;
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+- (UITableViewCell *)couchTableSource:(CouchUITableSource*)source cellForRowAtIndexPath:(NSIndexPath *)indexPath; {  
+  UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"ProjectCell"];
+  CouchQueryRow *row = [self.dataSource rowAtIndex:indexPath.row];
+  CouchDocument *doc = [row document];
+  cell.textLabel.text = doc.documentID;
   
-  NSDate *object = [_objects objectAtIndex:indexPath.row];
-  cell.textLabel.text = [object description];
-    return cell;
+  return cell;
 }
 
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)couchTableSource:(CouchUITableSource*)source updateFromQuery:(CouchLiveQuery*)query previousRows:(NSArray *)oldRows
 {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
+  BOOL (^isModified)(id, id) = ^ BOOL (id oldObj, id newObj) {
+    return ! [[oldObj documentRevision] isEqualToString:[newObj documentRevision]];
+  };
+  
+  NSArray *newRows = query.rows.allObjects;
+  NSArray *addedIndexPaths = [self addedIndexPathsOldRows:oldRows newRows:newRows];
+  NSArray *deletedIndexPaths = [self deletedIndexPathsOldRows:oldRows newRows:newRows];
+  NSArray *modifiedIndexPaths = [self modifiedIndexPathsOldRows:oldRows newRows:newRows usingBlock:^BOOL(id oldObj, id newObj) {
+    return isModified(oldObj, newObj);
+  }];
+  
+  [self.tableView beginUpdates];
+  [self.tableView insertRowsAtIndexPaths:addedIndexPaths withRowAnimation:UITableViewRowAnimationTop];
+  [self.tableView deleteRowsAtIndexPaths:deletedIndexPaths withRowAnimation:UITableViewRowAnimationBottom];
+  [self.tableView reloadRowsAtIndexPaths:modifiedIndexPaths withRowAnimation:UITableViewRowAnimationRight];
+  [self.tableView endUpdates];
 }
 
+
+- (void)couchTableSource:(CouchUITableSource*)source
+         operationFailed:(RESTOperation*)op
+{
+  [self failedWithError:op.error];
+}
+
+
+#pragma mark - Seque
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -272,6 +275,78 @@
     NSLog(@"KVO for %@, new conf: %@", keyPath, [[Globals sharedInstance] currentConfiguration].displayName);
     [self configurationChanged];
   }
+}
+
+
+#pragma mark - Table view update helpers
+
+
+-(NSDictionary *)indexMapForRows:(NSArray *)rows {
+  NSMutableDictionary *indexMap = [[NSMutableDictionary alloc] initWithCapacity:[rows count]];
+  for (int index = 0; index < [rows count]; ++index) {
+    CouchQueryRow *row = [rows objectAtIndex:index];
+    [indexMap setObject:[NSIndexPath indexPathForRow:index inSection:0] forKey:[row documentID]];
+  }
+  return indexMap;
+}
+
+
+-(NSArray *)deletedIndexPathsOldRows:(NSArray *)oldRows newRows:(NSArray *)newRows {
+  NSDictionary *oldIndexMap = [self indexMapForRows:oldRows];
+  NSDictionary *newIndexMap = [self indexMapForRows:newRows];
+  
+  NSMutableSet *remainder = [NSMutableSet setWithArray:oldIndexMap.allKeys];
+  NSSet *newIds = [NSSet setWithArray:newIndexMap.allKeys];
+  [remainder minusSet:newIds];
+  NSArray *deletedIds = remainder.allObjects;
+  
+  NSArray *deletedIndexPaths = [oldIndexMap objectsForKeys:deletedIds notFoundMarker:[NSNull null]];  
+  return deletedIndexPaths;
+}
+
+
+-(NSArray *)addedIndexPathsOldRows:(NSArray *)oldRows newRows:(NSArray *)newRows {
+  NSDictionary *oldIndexMap = [self indexMapForRows:oldRows];
+  NSDictionary *newIndexMap = [self indexMapForRows:newRows];
+  
+  NSMutableSet *remainder = [NSMutableSet setWithArray:newIndexMap.allKeys];
+  NSSet *oldIds = [NSSet setWithArray:oldIndexMap.allKeys];
+  [remainder minusSet:oldIds];
+  NSArray *addedIds = remainder.allObjects;
+  
+  NSArray *addedIndexPaths = [newIndexMap objectsForKeys:addedIds notFoundMarker:[NSNull null]];  
+  return addedIndexPaths;
+}
+
+
+-(NSArray *)modifiedIndexPathsOldRows:(NSArray *)oldRows newRows:(NSArray *)newRows usingBlock:(BOOL (^)(id, id))isModified {
+  NSDictionary *oldIndexMap = [self indexMapForRows:oldRows];
+  NSDictionary *newIndexMap = [self indexMapForRows:newRows];
+  
+  NSMutableSet *intersection = [NSMutableSet setWithArray:oldIndexMap.allKeys];
+  [intersection intersectSet:[NSSet setWithArray:newIndexMap.allKeys]];
+  NSArray *intersectionIds = intersection.allObjects;
+  
+  NSArray *intersectionOldIndexPaths = [oldIndexMap objectsForKeys:intersectionIds notFoundMarker:[NSNull null]];
+  NSArray *intersectionNewIndexPaths = [newIndexMap objectsForKeys:intersectionIds notFoundMarker:[NSNull null]];
+  NSAssert([intersectionIds count] == [intersectionOldIndexPaths count] &&
+           [intersectionIds count] == [intersectionNewIndexPaths count],
+           @"intersection index counts must be equal");
+  
+  NSMutableArray *modifiedIndexPaths = [NSMutableArray array];
+  for (NSUInteger index = 0; index < [intersectionIds count]; ++index) {
+    NSIndexPath *oldIndexPath = [intersectionOldIndexPaths objectAtIndex:index];
+    NSIndexPath *newIndexPath = [intersectionNewIndexPaths objectAtIndex:index];
+    CouchQueryRow *oldRow = [oldRows objectAtIndex:oldIndexPath.row];
+    CouchQueryRow *newRow = [newRows objectAtIndex:newIndexPath.row];
+    NSAssert([[oldRow documentID] isEqualToString:[newRow documentID]],
+             @"document ids must be equal for objects in intersection");
+    if (isModified(oldRow, newRow)) {
+      [modifiedIndexPaths addObject:newIndexPath];
+    }
+  }
+  
+  return modifiedIndexPaths;
 }
 
 
