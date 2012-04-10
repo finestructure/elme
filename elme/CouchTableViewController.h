367cbeae301f33254c2ba3843aa31a7dc045907b
Sven A. Schmidt <sas@abstracture.de>
2012-04-10 14:02:55 +0200

    Added CouchTableViewController

---
+++ elme/CouchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CouchTableViewController.h
+//  elme
+//
+//  Created by Sven A. Schmidt on 10.04.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CouchTableViewController : UIViewController
+
+@end
