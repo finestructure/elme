e6134bbde20bf2ced27dd7f6507041b52d48765c
Sven A. Schmidt <sas@abstracture.de>
2012-04-11 10:52:56 +0200

    Added EditIncidentViewController

---
+++ elme/EditIncidentViewController.h
@@ -0,0 +1,16 @@
+//
+//  EditIncidentViewController.h
+//  elme
+//
+//  Created by Sven A. Schmidt on 11.04.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditIncidentViewController : UIViewController
+
+- (IBAction)save:(id)sender;
+- (IBAction)cancel:(id)sender;
+
+@end
---
+++ elme/EditIncidentViewController.m
@@ -0,0 +1,52 @@
+//
+//  EditIncidentViewController.m
+//  elme
+//
+//  Created by Sven A. Schmidt on 11.04.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "EditIncidentViewController.h"
+
+@interface EditIncidentViewController ()
+
+@end
+
+@implementation EditIncidentViewController
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+  [super viewDidUnload];
+  // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)save:(id)sender {
+  [self dismissModalViewControllerAnimated:YES];
+}
+
+- (IBAction)cancel:(id)sender {
+  [self dismissModalViewControllerAnimated:YES];
+}
+
+@end
