3a20c6905ccc7376b440ef4951da382ec64e549a
Sven A. Schmidt <sas@abstracture.de>
2012-04-18 13:11:22 +0200

    Populating map view

--- elme/LocationCell.m
+++ elme/LocationCell.m
@@ -8,6 +8,175 @@
 
 #import "LocationCell.h"
 
+#import "Annotation.h"
+#import "Database.h"
+#import "Project.h"
+
+#import <CouchCocoa/CouchCocoa.h>
+#import <CouchCocoa/CouchDesignDocument_Embedded.h>
+
+
 @implementation LocationCell
+
+@synthesize mapView = _mapView;
+
+
+#pragma mark - Helpers
+
+
+- (void)zoomMapToAnnotationRegion {
+  MKMapRect zoomRect = MKMapRectNull;
+  for (id <MKAnnotation> annotation in self.mapView.annotations)
+  {
+    MKMapPoint annotationPoint = MKMapPointForCoordinate(annotation.coordinate);
+    MKMapRect pointRect = MKMapRectMake(annotationPoint.x, annotationPoint.y, 0, 0);
+    if (MKMapRectIsNull(zoomRect)) {
+      zoomRect = pointRect;
+    } else {
+      zoomRect = MKMapRectUnion(zoomRect, pointRect);
+    }
+  }
+  [self.mapView setVisibleMapRect:zoomRect animated:YES];
+}
+
+
+- (void)addAnnotationWithTitle:(NSString *)title subtitle:(NSString *)subtitle coordinate:(CLLocationCoordinate2D)coordinate
+{
+  Annotation *ann = [[Annotation alloc] init];
+  ann.title = title;
+  ann.subtitle = subtitle;
+  ann.coordinate = coordinate;
+  [self.mapView addAnnotation:ann];
+  [self zoomMapToAnnotationRegion];
+}
+
+
+- (void)updateProjectWithId:(NSString *)projectId withCoordinate:(CLLocationCoordinate2D)coordinate forAddress:(NSString *)address
+{
+  Project *project = [[Database sharedInstance] projectWithId:projectId];
+  BOOL needsUpdate = NO;
+  NSMutableArray *newUnits = [project.units mutableCopy];
+  
+  for (NSUInteger index = 0; index < project.units.count; index++) {
+    NSDictionary *unit = [project.units objectAtIndex:index];
+    NSString *unitAddress = [unit valueForKey:@"address"];
+    if ([address isEqualToString:unitAddress]
+        && [[unit objectForKey:@"latitude"] doubleValue] != coordinate.latitude
+        && [[unit objectForKey:@"longitude"] doubleValue] != coordinate.longitude
+        ) {
+      NSMutableDictionary *newUnit = [unit mutableCopy];
+      [newUnit setValue:[NSNumber numberWithDouble:coordinate.longitude] forKey:@"longitude"];
+      [newUnit setValue:[NSNumber numberWithDouble:coordinate.latitude] forKey:@"latitude"];
+      [newUnits replaceObjectAtIndex:index withObject:newUnit];
+      needsUpdate = YES;
+    }
+  }
+  if (needsUpdate) {
+    project.units = newUnits;
+    RESTOperation *op = [project save];
+    [op onCompletion: ^{
+      if (op.error) {
+        NSLog(@"Error while saving coordinates: %@", [op.error localizedDescription]);
+      }
+    }];
+    [op start];
+  }
+}
+
+
+- (void)setPinForUnit:(CouchQueryRow *)row {
+  NSDictionary *unit = row.key;
+  NSDictionary *project = row.value;
+  NSString *title = [unit objectForKey:@"name"];
+  NSString *subtitle = [unit objectForKey:@"address"];
+  
+  if ([unit objectForKey:@"latitude"] && [unit objectForKey:@"longitude"]) {
+    CLLocationDegrees latitude = [[unit objectForKey:@"latitude"] doubleValue];
+    CLLocationDegrees longitude = [[unit objectForKey:@"longitude"] doubleValue];
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    [self addAnnotationWithTitle:title subtitle:subtitle coordinate:coordinate];
+  } else {
+    CLGeocoder *coder = [[CLGeocoder alloc] init];
+    NSString *address = [unit objectForKey:@"address"];
+    [coder geocodeAddressString:address completionHandler:^(NSArray *__strong placemarks, NSError *__strong error) {
+      if (error != nil) {
+        if ([error code] == kCLErrorGeocodeFoundNoResult) {
+          NSLog(@"*** no result found *** (for: %@)", address);
+        } else {
+          NSLog(@"geocoding error: %@", [error localizedDescription]);
+        }
+      } else if ([placemarks count] > 0) {
+        CLPlacemark *best = [placemarks objectAtIndex:0];
+        NSLog(@"Best placemark: %@", best);
+        CLLocationCoordinate2D coordinate = best.location.coordinate;
+        [self addAnnotationWithTitle:title subtitle:subtitle coordinate:coordinate];
+        { // save coordinates to unit
+          [self updateProjectWithId:[project valueForKey:@"_id"] withCoordinate:coordinate forAddress:address];
+        }
+      }
+    }];
+    
+  }
+}
+
+
+- (void)showUnits {
+  CouchDesignDocument* design = [[Database sharedInstance] designDocumentWithName: @"default"];
+  [design defineViewNamed: @"units" 
+                 mapBlock: ^(NSDictionary* doc, void (^emit)(id key, id value)) {
+                   id type = [doc objectForKey: @"type"];
+                   if (type && [type isEqualToString:@"project"]) {
+                     NSArray *units = [doc objectForKey:@"units"];
+                     if (units) {
+                       for (NSDictionary *unit in units) {
+                         emit(unit, doc);
+                       }
+                     }
+                   }
+                 } 
+                  version: @"2.0"];
+  CouchQuery *query = [design queryViewNamed:@"units"];
+  RESTOperation *op = [query start];
+  [op onCompletion:^{
+    CouchQueryEnumerator *rows = op.resultObject;
+    CouchQueryRow *row = nil;
+    while ((row = rows.nextRow) != nil) {
+      NSLog(@"row: %@", row);
+      [self setPinForUnit:row];
+    }
+  }];
+}
+
+
+#pragma mark - KVO
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath  
+                      ofObject:(id)object  
+                        change:(NSDictionary *)change  
+                       context:(void *)context {  
+  if ([self.mapView showsUserLocation]) {  
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(self.mapView.userLocation.coordinate, 2000, 2000);    
+    [self.mapView setRegion:region animated:YES];
+  }
+}
+
+
+#pragma mark - Init & view lifecycle
+
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+  self = [super initWithCoder:decoder];
+  if (self != nil) {
+    [self.mapView.userLocation addObserver:self  
+                                forKeyPath:@"location"  
+                                   options:(NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld)  
+                                   context:NULL];
+    [self showUnits];
+  }
+  return self;
+}
+
 
 @end
