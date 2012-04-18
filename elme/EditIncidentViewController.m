42bf186e8b970e2d6df76086d39be5b88674d733
Sven A. Schmidt <sas@abstracture.de>
2012-04-18 13:11:03 +0200

    Removed commented code

--- elme/EditIncidentViewController.m
+++ elme/EditIncidentViewController.m
@@ -13,8 +13,6 @@
 #import "Incident.h"
 #import "LocationCell.h"
 
-#import <CouchCocoa/CouchDesignDocument_Embedded.h>
-
 
 @interface EditIncidentViewController () {
   NSMutableArray *cells;
@@ -69,101 +67,6 @@
 }
 
 
-//- (void)zoomMapToAnnotationRegion {
-//  MKMapRect zoomRect = MKMapRectNull;
-//  for (id <MKAnnotation> annotation in self.mapView.annotations)
-//  {
-//    MKMapPoint annotationPoint = MKMapPointForCoordinate(annotation.coordinate);
-//    MKMapRect pointRect = MKMapRectMake(annotationPoint.x, annotationPoint.y, 0, 0);
-//    if (MKMapRectIsNull(zoomRect)) {
-//      zoomRect = pointRect;
-//    } else {
-//      zoomRect = MKMapRectUnion(zoomRect, pointRect);
-//    }
-//  }
-//  [self.mapView setVisibleMapRect:zoomRect animated:YES];
-//}
-//
-//
-//- (void)addAnnotationWithTitle:(NSString *)title subtitle:(NSString *)subtitle coordinate:(CLLocationCoordinate2D)coordinate
-//{
-//  Annotation *ann = [[Annotation alloc] init];
-//  ann.title = title;
-//  ann.subtitle = subtitle;
-//  ann.coordinate = coordinate;
-//  [self.mapView addAnnotation:ann];
-//  [self zoomMapToAnnotationRegion];
-//}
-//
-//
-//- (void)setPinForUnit:(CouchQueryRow *)row {
-//  NSDictionary *unit = row.value;
-//  NSString *title = [unit objectForKey:@"name"];
-//  NSString *subtitle = [unit objectForKey:@"address"];
-//  
-//  if ([unit objectForKey:@"latitude"] && [unit objectForKey:@"longitude"]) {
-//    CLLocationDegrees latitude = [[unit objectForKey:@"latitude"] doubleValue];
-//    CLLocationDegrees longitude = [[unit objectForKey:@"longitude"] doubleValue];
-//    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(latitude, longitude);
-//    [self addAnnotationWithTitle:title subtitle:subtitle coordinate:coordinate];
-//  } else {
-//    CLGeocoder *coder = [[CLGeocoder alloc] init];
-//    NSString *address = [unit objectForKey:@"address"];
-//    [coder geocodeAddressString:address completionHandler:^(NSArray *__strong placemarks, NSError *__strong error) {
-//      if (error != nil) {
-//        if ([error code] == kCLErrorGeocodeFoundNoResult) {
-//          NSLog(@"*** no result found *** (for: %@)", address);
-//        } else {
-//          NSLog(@"geocoding error: %@", [error localizedDescription]);
-//        }
-//      } else if ([placemarks count] > 0) {
-//        CLPlacemark *best = [placemarks objectAtIndex:0];
-//        NSLog(@"Best placemark: %@", best);
-//        CLLocationCoordinate2D coordinate = best.location.coordinate;
-//        [self addAnnotationWithTitle:title subtitle:subtitle coordinate:coordinate];
-//        { // save coordinates to unit
-//          NSMutableDictionary *newUnit = [unit mutableCopy];
-//          [newUnit setValue:[NSNumber numberWithDouble:coordinate.longitude] forKey:@"longitude"];
-//          [newUnit setValue:[NSNumber numberWithDouble:coordinate.latitude] forKey:@"latitude"];
-//#warning save coordinates
-//          //          [self updateUnitAtIndex:index withObject:newUnit];
-//        }
-//      }
-//    }];
-//    
-//  }
-//}
-//
-//
-//- (void)showUnits {
-//  CouchDesignDocument* design = [[Database sharedInstance] designDocumentWithName: @"default"];
-//  [design defineViewNamed: @"units" 
-//                 mapBlock: ^(NSDictionary* doc, void (^emit)(id key, id value)) {
-//                   id type = [doc objectForKey: @"type"];
-//                   if (type && [type isEqualToString:@"project"]) {
-//                     NSArray *units = [doc objectForKey:@"units"];
-//                     if (units) {
-//                       for (NSDictionary *unit in units) {
-//                         id name = [unit objectForKey:@"name"];
-//                         emit(name, unit);
-//                       }
-//                     }
-//                   }
-//                 } 
-//                  version: @"1.0"];
-//  CouchQuery *query = [design queryViewNamed:@"units"];
-//  RESTOperation *op = [query start];
-//  [op onCompletion:^{
-//    CouchQueryEnumerator *rows = op.resultObject;
-//    CouchQueryRow *row = nil;
-//    while ((row = rows.nextRow) != nil) {
-//      NSLog(@"row: %@", row);
-//      [self setPinForUnit:row];
-//    }
-//  }];
-//}
-
-
 #pragma mark - Init and view lifecycle
 
 
@@ -199,10 +102,6 @@
 - (void)viewWillAppear:(BOOL)animated {
   [super viewWillAppear:animated];
   
-//  [self.mapView.userLocation addObserver:self  
-//                              forKeyPath:@"location"  
-//                                 options:(NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld)  
-//                                 context:NULL];
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
   [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
@@ -211,7 +110,6 @@
 - (void)viewWillDisappear:(BOOL)animated {
   [super viewWillDisappear:animated];
   
-//  [self.mapView.userLocation removeObserver:self forKeyPath:@"location"];
   [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
   [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
@@ -300,20 +198,6 @@
       break;
   }
 }
-
-
-#pragma mark - KVO
-
-
-//- (void)observeValueForKeyPath:(NSString *)keyPath  
-//                      ofObject:(id)object  
-//                        change:(NSDictionary *)change  
-//                       context:(void *)context {  
-//  if ([self.mapView showsUserLocation]) {  
-//    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(self.mapView.userLocation.coordinate, 2000, 2000);    
-//    [self.mapView setRegion:region animated:YES];
-//  }
-//}
 
 
 #pragma mark - UITextViewDelegate
