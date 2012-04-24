2f4d842488b28828bc42f1e25b574476cb984987
Sven A. Schmidt <sas@abstracture.de>
2012-04-24 12:22:09 +0200

    Added CraftsViewController

---
+++ elme/CraftsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CraftsViewController.h
+//  elme
+//
+//  Created by Sven A. Schmidt on 24.04.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CraftsViewController : UITableViewController
+
+@end
